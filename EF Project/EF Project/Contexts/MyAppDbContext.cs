022b585714503bcf91055cea37abcb372bb3b885
Mar0 <106659477+MarO-cmd@users.noreply.github.com>
2025-02-19 20:44:51 +0200

    Add files via upload

---
+++ EF Project/EF Project/Contexts/MyAppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EF_Project.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Project.Contexts
+{
+    internal class MyAppDbContext:DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Student> Students{ get; set; }
+    }
+}
---
+++ EF Project/EF Project/Entities/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Project.Entities
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Hiring_Date { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+        public int Inst_Id { get; set; }
+        public virtual Instructor Instructor{ get; set; }
+
+
+
+
+    }
+
+}
